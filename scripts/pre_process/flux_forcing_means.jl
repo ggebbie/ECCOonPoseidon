8a4d8ecd07f0168e8f345d0de0438e870c84abee
cora-a-hersh <80273585+cora-a-hersh@users.noreply.github.com>
2024-07-26 13:33:25 -0400

    Update flux_forcing_means.jl
    add plotting

--- scripts/pre_process/flux_forcing_means.jl
+++ scripts/pre_process/flux_forcing_means.jl
@@ -37,7 +37,8 @@ varnames = ("atmPload","oceFWflx","oceQsw","oceSflux","oceSPflx","oceTAUE","oceT
 years = 1992:2017
 
 #calculate mean from original flux files
-for vname ∈ varnames
+vname = varnames[2]
+#for vname ∈ varnames
     filein = joinpath(inputdir,vname*midname)
     println(filein)
     #initialize matrix to hold added fields
@@ -51,30 +52,88 @@ for vname ∈ varnames
 
     #add each year of flux data together, then add to meanout
     @time for tt in 1:length(years)
-        fname = filein*string(years[tt])
-        field = read_bin(fname,Float32,γ);
-        fname_exp = filein_exp*string(years[tt])
-        field_exp = read_bin(fname_exp,Float32,γ);
+        fname = filein*string(years[tt]) #get filename for this variable and year (original fluxes)
+        field = read_bin(fname,Float32,γ); #read in data
+        fname_exp = filein_exp*string(years[tt]) #get filename for this variable and year (modified fluxes)
+        field_exp = read_bin(fname_exp,Float32,γ); #read in data
         for fv in 1:5 #for each of the five grid faces
-            tmplat = ϕC[fv];
-            tmplon = λC[fv];
+            tmplat = ϕC[fv]; #get lat grid
+            tmplon = λC[fv]; #get lon grid
             l1 = size(meanout[fv],1);
             l2 = size(meanout[fv],2);
-            tmptot = fill(0.0, l1, l2); #empty array to hold added fluxes for this grid face
+            tmptot = fill(0.0, l1, l2); #array of zeros to hold added fluxes for this grid face in this year
             tmptot_exp = fill(0.0, l1, l2);
-            for ss in 1:size(field,2) #for each 6 hour timestep
+            for ss in 1:size(field,2) #for each 6 hour timestep in this year
                 field_face = field[fv,ss]; #lon/lat grid of flux for this timestep
-                tmptot = tmptot .+ field_face;
+                tmptot .+= field_face; 
                 field_face_exp = field_exp[fv,ss];
-                tmptot_exp = tmptot_exp .+ field_face_exp;
+                tmptot_exp .+= field_face_exp;
             end
-            meanout[fv] = meanout[fv] .+ tmptot;
-            meanout_exp[fv] = meanout_exp[fv] .+ tmptot_exp;
+            meanout.f[fv] .+= tmptot;
+            meanout_exp.f[fv] .+= tmptot_exp;
         end
     end
     meanout = meanout./37987; #divide by total number of 6 hour timesteps in 26 year timeseries
     meanout_exp = meanout_exp./37987; #divide by total number of 6 hour timesteps in 26 year timeseries
     mean_diff = meanout_exp .- meanout;
+
+    mean_diff_regpoles = regularpoles(mean_diff,γ,rp_params)
+    meanout_regpoles = regularpoles(meanout,γ,rp_params)
+    meanout_exp_regpoles = regularpoles(meanout_exp,γ,rp_params)
+
+    figure()
+    clf()
+    cmap_seismic =get_cmap("seismic")
+    lims = range(-1.0,step=0.05,stop=1.0)
+    contourf(rp_params.λC,
+    rp_params.ϕC,
+    mean_diff_regpoles',
+    lims,
+    cmap=cmap_seismic)
+    colorbar(label="weight",orientation="vertical",ticks=lims)
+    !ispath(plotsdir()) && mkpath(plotsdir())
+    outfname = plotsdir("meandiff.pdf")
+    xlbl = "longitude "*L"[\degree E]"
+    ylbl = "latitude "*L"[\degree N]"
+    xlabel(xlbl)
+    ylabel(ylbl)
+    savefig(outfname)
+
+    figure()
+    clf()
+    cmap_seismic =get_cmap("seismic")
+    lims = range(-200.0,step=10.0,stop=200.0)
+    contourf(rp_params.λC,
+    rp_params.ϕC,
+    meanout_regpoles',
+    lims,
+    cmap=cmap_seismic)
+    colorbar(label="weight",orientation="vertical",ticks=lims)
+    !ispath(plotsdir()) && mkpath(plotsdir())
+    outfname = plotsdir("meanout.pdf")
+    xlbl = "longitude "*L"[\degree E]"
+    ylbl = "latitude "*L"[\degree N]"
+    xlabel(xlbl)
+    ylabel(ylbl)
+    savefig(outfname)
+
+    figure()
+    clf()
+    cmap_seismic =get_cmap("seismic")
+    lims = range(-200.0,step=10.0,stop=200.0)
+    contourf(rp_params.λC,
+    rp_params.ϕC,
+    meanout_exp_regpoles',
+    lims,
+    cmap=cmap_seismic)
+    colorbar(label="weight",orientation="vertical",ticks=lims)
+    !ispath(plotsdir()) && mkpath(plotsdir())
+    outfname = plotsdir("meanout_exp.pdf")
+    xlbl = "longitude "*L"[\degree E]"
+    ylbl = "latitude "*L"[\degree N]"
+    xlabel(xlbl)
+    ylabel(ylbl)
+    savefig(outfname)
 
     #now load in each experiment flux file and subtract mean_diff, then re-save updated flux values
     for tt in 1:length(years)
@@ -87,7 +146,7 @@ for vname ∈ varnames
         println("saving file "*fnameout)
         write(fnameout,field_exp_fixmean)
     end
-end
+#end
 
 
 #bad individual timeseries method ----------------------------
